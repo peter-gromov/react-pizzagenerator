e4ec8b170b276d55abda9594177c238eb3e5d870
Petr Gromov <p.gromov@babadu.ru>
2018-01-08 22:48:41 +0300

    Config + Pizza Class

--- src/js/pizzagenerator.js
+++ src/js/pizzagenerator.js
@@ -1,43 +1,165 @@
-;
+"use strict";
 
-var _pg = {};
 
-_pg.settings = {};
+class PizzaConfig {
+	constructor(){
+		this.settings = {};
 
-_pg.settings.diameter = [
-	{name: '35 см', price: 100, id: '500'},
-	{name: '40 см', price: 150, id: '501'},
-	{name: '50 см', price: 350, id: '502'}
-];
 
-_pg.settings.width = [
-	{ name: 'Тонкое', price: 100, id: '101'},
-	{ name: 'Толстое', price: 150, id: '102'}
-];
-_pg.settings.base = [
-	{ name: 'Сливочный', price: 0, id: '700'},
-	{ name: 'Томатный', price: 0, id: '701'},
-	{ name: 'Грибной', price: 0, id: '702'}
-];
+		this.settings = {};
 
-_pg.settings.ingredients = [
-	{ name: 'Морской коктейль', price: 80 , id: '1001'},
-	{ name: 'Тигровая креветка', price: 70 , id: '1002'},
-	{ name: 'Бекон', price: 50 , id: '1003'},
-	{ name: 'Салями', price: 80 , id: '1004'},
-	{ name: 'Чорризо', price: 80 , id: '1005'},
-	{ name: 'Курица', price: 110 , id: '1006'},
-	{ name: 'Говядина', price: 80 , id: '1007'},
-	{ name: 'Халапеньо', price: 80 , id: '1008'},
-	{ name: 'Чеснок', price: 80 , id: '1009'},
-	{ name: 'Доп. майонез', price: 80 , id: '1010'}
-];
+		this.settings.diameter = [
+			{name: '35 см', price: 100, id: 500, weight: 300},
+			{name: '40 см', price: 150, id: 501, weight: 400},
+			{name: '50 см', price: 350, id: 502, weight: 600}
+		];
+
+		this.settings.width = [
+			{ name: 'Тонкое', price: 100, id: 101, weight: 100},
+			{ name: 'Толстое', price: 150, id: 102, weight: 200}
+		];
+		this.settings.base = [
+			{ name: 'Сливочный', price: 0, id: 700, weight: 0},
+			{ name: 'Томатный', price: 0, id: 701, weight: 0},
+			{ name: 'Грибной', price: 0, id: 702, weight: 0}
+		];
+
+		this.settings.ingredients = [
+			{ name: 'Морской коктейль', price: 80 , id: 1001, weight: 70},
+			{ name: 'Тигровая креветка', price: 70 , id: 1002, weight: 70},
+			{ name: 'Бекон', price: 50 , id: 1003, weight: 40},
+			{ name: 'Салями', price: 80 , id: 1004, weight: 40},
+			{ name: 'Чорризо', price: 80 , id: 1005, weight: 40},
+			{ name: 'Курица', price: 110 , id: 1006, weight: 70},
+			{ name: 'Говядина', price: 80 , id: 1007, weight: 70},
+			{ name: 'Халапеньо', price: 80 , id: 1008, weight: 50},
+			{ name: 'Чеснок', price: 80 , id: 1009, weight: 10},
+			{ name: 'Доп. майонез', price: 80 , id: 1010, weight: 25}
+		];
+	}
+
+	price(id, where){
+ 		return this.getRow(id,where).price;
+	}
+
+	name(id, where){
+		return this.getRow(id,where).name;
+	}
+
+	weight(id, where){
+		return this.getRow(id,where).weight;
+	}
+
+	getRow(id, where){
+		let block, r;
+
+		if( typeof id == 'undefined') return false;
+
+		if (typeof where == 'undefined' ){
+
+			//Look through whole settings 
+			for( block in this.settings ){
+				if( r = this.settings[block].find(function(el){ return el.id === id }) ){
+					return r;
+				}
+			}
+
+		}else{
+
+			//Look in defined block
+			if( typeof this.settings[where] != 'undefined' ){
+				return this.settings[where].find(function(el){ return el.id === id });
+			}			
+		}
+
+		return false;
+	}
+}
+var pizzaConfig = new PizzaConfig();
+
+
+class Pizza {
+	constructor ( diameter, width, base, ingredients ){
+		this.diameter = diameter || 500;
+		this.width = width || 101;
+		this.base = base || 700;
+		this.ingredients = ingredients || [];
+	}
+
+
+	//Get price of the pizza through config
+	get price (){
+
+		let totalPrice = 0,
+			ingredient;
+
+		totalPrice +=  pizzaConfig.price ( this.diameter ) ;
+		totalPrice +=  pizzaConfig.price ( this.width ) ;
+		totalPrice +=  pizzaConfig.price ( this.base ) ;
+		
+		//Добавляем цену ингредиентов
+		for( ingredient of this.ingredients ){
+			totalPrice += pizzaConfig.price(ingredient.id) * ingredient.quantity;
+		}
+
+
+		return totalPrice;
+	}
+
+
+	//Get price of the pizza through config
+	get weight (){
+
+		let totalWeight = 0,
+			ingredient;
+
+		totalWeight +=  pizzaConfig.weight ( this.diameter ) ;
+		totalWeight +=  pizzaConfig.weight ( this.width ) ;
+		totalWeight +=  pizzaConfig.weight ( this.base ) ;
+		
+		//Добавляем цену ингредиентов
+		for( ingredient of this.ingredients ){
+			totalWeight += pizzaConfig.weight(ingredient.id) * ingredient.quantity;
+		}
+
+
+		return totalWeight;
+	}
+
+
+	//Get name of pizza thgough config
+	get name (){
+		let pizzaName = '',
+			ingredients = [],
+			i;
+
+		pizzaName += 'Пицца ' + pizzaConfig.name(this.diameter) +
+						', '  + pizzaConfig.name(this.width).toLowerCase() + ' тесто' +
+						', '  + pizzaConfig.name(this.base).toLowerCase() + ' соус';
+
+
+		if( this.ingredients.length > 0 ){
+			pizzaName += ', доп.: ';
+			for(i of this.ingredients ){
+				ingredients[ ingredients.length ] = pizzaConfig.name(i.id).toLowerCase() + (i.quantity > 1? ' ('+i.quantity+' шт.)' : '');
+			}
+			pizzaName += ingredients.join(', ');
+
+		}
+
+		return pizzaName;
+	}
+};
+
+
+var PizzaGen = {};
+
 
 //Текущая пицца
-_pg.canvas = {};
+PizzaGen.canvas = {};
 
 //Отложенный заказ
-_pg.order = [
+PizzaGen.order = [
 	/*
 		{
 			quantity: 1,
@@ -57,31 +179,14 @@ _pg.order = [
 ];
 
 
+ 
 
-_pg.getPizzaPrice = function(pizza){
-	var totalPrice = 0;
-
-	totalPrice += (typeof pizza.diameter != 'undefined') ? _pg.settings.diameter.find(function(el){ return el.id == pizza.diameter }).price :0;
-	totalPrice += (typeof pizza.width != 'undefined') ? _pg.settings.width.find(function(el){ return el.id == pizza.width }).price :0;
-	totalPrice += (typeof pizza.base != 'undefined') ? _pg.settings.base.find(function(el){ return el.id == pizza.base }).price :0;
-
-	//Добавляем цену ингредиентов
-	for(var i in pizza.ingredients ){
-		if( typeof pizza.ingredients[i] == 'undefined ') continue;
-		totalPrice += _pg.settings.ingredients.find( function(el) { return el.id == pizza.ingredients[i].id; }).price * pizza.ingredients[i].quantity;
-	}
-
-
-	return totalPrice;
-
-};
-
-
-_pg.getTotalPrice = function(order){
+PizzaGen.getTotalPrice = function(order){
 	var totalPrice = 0;
 
 	for(var k in order){
-		totalPrice += this.getPizzaPrice( this.order[k] );
+		//console.log( order[k] );
+		totalPrice += this.getPizzaPrice(  order[k].pizza ) *  order[k].quantity  ;
 	}
 
 	return totalPrice;
--- test/pizzagenerator.test.js
+++ test/pizzagenerator.test.js
@@ -6,7 +6,7 @@ describe("Тест расчётов - пицца", function(){
 
 	it('Пустая пицца 35 см без всего - тонкое тесто', function () {		 
 		assert.equal(
-			_pg.getPizzaPrice({
+			PizzaGen.getPizzaPrice({
 					diameter: 500, 
 					width: 101, 
 					base: 700, 
@@ -18,7 +18,7 @@ describe("Тест расчётов - пицца", function(){
 
 	it('Пустая пицца 40 см без всего - толстое тесто', function () {
 		assert.equal( 
-			_pg.getPizzaPrice({
+			PizzaGen.getPizzaPrice({
 					diameter: 501, 
 					width: 102, 
 					base: 700, 
@@ -31,7 +31,7 @@ describe("Тест расчётов - пицца", function(){
 	it('Пицца 50 см - толстое тесто - бекон * 2 + халапеньо', function () {
 
 		assert.equal( 
-			_pg.getPizzaPrice({
+			PizzaGen.getPizzaPrice({
 					diameter: 502, 
 					width: 101, 
 					base: 700, 
@@ -46,7 +46,7 @@ describe("Тест расчётов - пицца", function(){
 	});	
 	it('Пицца 35 см - тонкое тесто - Морской коктейль, говядина, майонез', function () {		 
 		assert.equal( 
-			_pg.getPizzaPrice({
+			PizzaGen.getPizzaPrice({
 					diameter: 500, 
 					width: 101, 
 					base: 700, 
@@ -67,7 +67,7 @@ describe("Тест расчётов заказов", function(){
 
 	it('Заказ: пустая пицца 35 см, тонкая, без всего X 1 шт.', function () {		 
 		assert.equal( 
-			_pg.getTotalPrice([ 
+			PizzaGen.getTotalPrice([ 
 					{
 						quantity: 1,
 						pizza: {
@@ -78,12 +78,13 @@ describe("Тест расчётов заказов", function(){
 						}
 					}
 				]),
+
 			 200 ); 	
 	});	
 
 	it('Заказ: Пицца 40 см тонкое, бекон 2 шт. Х 2 шт.', function () {		 
 		assert.equal( 
-			_pg.getTotalPrice([ 
+			PizzaGen.getTotalPrice([ 
 					{
 						quantity: 2,
 						pizza: {
@@ -94,13 +95,14 @@ describe("Тест расчётов заказов", function(){
 						}
 					}
 				]),
+
 			 700 ); 	
 	});	
 
-	it('Заказ: пустая пицца 40 см тонкое, бекон 2 шт. Х 2 шт.', function () {		 
+	it('Заказ: пицца 40 см тонкое, бекон 2 шт. Х 2 шт. + 50 см, толстое,  ', function () {		 
 		assert.equal( 
-			_pg.getTotalPrice([ 
-					{
+			PizzaGen.getTotalPrice([ 
+					{ //700
 						quantity: 2,
 						pizza: {
 							diameter: 501, 
@@ -109,7 +111,7 @@ describe("Тест расчётов заказов", function(){
 							ingredients: [ {id:1003, quantity: 2}]
 						}
 					},
-					{
+					{ //500
 						quantity: 1,
 						pizza: {
 							diameter: 502, 
@@ -119,9 +121,36 @@ describe("Тест расчётов заказов", function(){
 						}
 					}
 				]),
-			 350 ); 	
+
+			 1200 ); 	
 	});	
 
+	 
+
+});
+
+
+
+describe("SETTINGS TEST", function(){
+
+	it('row func: Поиск настроек по ID 1003 - бекон ', function () {	assert.equal( pizzaConfig.getRow( 1003).name, 'Бекон' ); });	
+	it('row func: Поиск настроек по ID 500 - 35 см ', function () { assert.equal( pizzaConfig.getRow( 500).name, '35 см' ); });	
+
+	it('row func: Поиск настроек по ID 500 - 35 см ', function () { assert.equal( pizzaConfig.price( 500), 100 ); });	
+	it('row func: Поиск настроек по ID 500 - 35 см ', function () { assert.equal( pizzaConfig.weight( 500), 300 ); });	
+
+	it('row func: Поиск настроек по ID 500 - 35 см ', function () { assert.equal( pizzaConfig.weight( 500, 'diameter'), '300' ); });	
+		
+	//Проверка функций объекта пиццы
+	it('Пицца - Имя: Пицца 35 см, тонкое тесто, сливочный соус', function () { assert.equal( new Pizza().name, 'Пицца 35 см, тонкое тесто, сливочный соус' ); });	
+	it('Пицца - Имя: Пицца 35 см, тонкое тесто, сливочный соус, доп.: морской коктейль (2 шт.), говядина', function () { assert.equal( new Pizza(false,false,false, [{id: 1001, quantity:2}, {id: 1007, quantity: 1}]).name, 'Пицца 35 см, тонкое тесто, сливочный соус, доп.: морской коктейль (2 шт.), говядина' ); });	
+
+	it('Пицца - Вес: 400', function () { assert.equal( new Pizza().weight, '400' ); });	
+	it('Пицца - Вес: 610', function () { assert.equal( new Pizza(false,false,false, [{id: 1001, quantity:2}, {id: 1007, quantity: 1}]).weight, '610' ); });	
+
+	it('Пицца - цена: 610', function () { assert.equal( new Pizza(false,false,false).price, '200' ); });	
+	it('Пицца - цена: 610', function () { assert.equal( new Pizza(false,false,false, [{id: 1001, quantity:2}, {id: 1007, quantity: 1}]).price, '440' ); });	
+ 
 	 
 
 });
